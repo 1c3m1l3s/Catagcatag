505f02b2d1db01faaaec730f9c093a0775dcc2aa
Ic Myles Catagcatag <iccatagcatag@gmail.com>
2025-12-05 12:07:16 +0800

    erased dark mode

--- home.js
+++ home.js
@@ -18,20 +18,6 @@ window.addEventListener('scroll', () => {
   });
 });
 
-// DARK MODE
-const toggleBtn = document.getElementById('theme-toggle');
-
-toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('light-mode');
-
-    // Change icon depending on theme
-    if(document.body.classList.contains('light-mode')){
-        toggleBtn.textContent = '🌞'; // Sun for light mode
-    } else {
-        toggleBtn.textContent = '🌙'; // Moon for dark mode
-    }
-});
-
 // SLIDE-IN IMAGE (ABOUT / SKILLS SECTION)
 
 document.addEventListener('DOMContentLoaded', () => {
